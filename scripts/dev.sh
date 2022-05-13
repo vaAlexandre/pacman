02fa80a693f460b0c1634acc1ee0cde007dfb770
Sylvain Huguet <sylvain@kasten.io>
2022-05-13 18:46:39 +0200

    moving to alpine based mongo to reduce threat

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -68,7 +68,7 @@ docker run --rm -d --name ${MONGO_CONTAINER_NAME} \
 #     -e MONGO_AUTH_PWD=${MONGO_AUTH_PWD} \
 #     -v ${LOCAL_WORKDIR}:${PACMAN_WORKDIR} \
 #     -p ${PACMAN_LOCAL_PORT}:${PACMAN_CONTAINER_PORT} \
-#     --entrypoint bash \
+#     --entrypoint ash \
 #     mongo -- mongo --host ${MONGO_SERVICE_HOST} -u ${MONGO_AUTH_USER} -p ${MONGO_AUTH_PWD} --authenticationDatabase admin pacman
 
 docker run --rm -it --name ${PACMAN_CONTAINER_NAME} \
@@ -78,5 +78,5 @@ docker run --rm -it --name ${PACMAN_CONTAINER_NAME} \
     -e MONGO_AUTH_PWD=${MONGO_AUTH_PWD} \
     -v ${LOCAL_WORKDIR}:${PACMAN_WORKDIR} \
     -p ${PACMAN_LOCAL_PORT}:${PACMAN_CONTAINER_PORT} \
-    --entrypoint bash \
+    --entrypoint ash \
     ${PACMAN_CONTAINER_IMAGE}
