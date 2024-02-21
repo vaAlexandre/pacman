9ca7a232cede305106d45de33a2b5fe4d482aa0d
Sylvain Huguet <sylvain@huguet.me>
2024-02-21 16:04:23 +0100

    V0.1.13 (#18)
    * General house keeping:
    - Update chart to leverage Bitnami MongoDB 14.10.1
    - Fix various JS vulnerabilities
    - Update the dev scripts
    - Add a publish script with steps to publish new image
    - Update Helm dependencies

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,13 +10,28 @@
 #
 # Required tools:
 #   - docker
-NEW_VERSION='0.1.12'
+NEW_VERSION='0.1.13'
 GITHUB_USERNAME='shuguet'
 GITHUB_TOKEN='nice try'
 
+# Perform local build
 docker build . -t pacman
+
+# Update Helm dependencies (prepare git commit)
+(cd charts/pacman && helm dependency update)
+git add .
+git commit -m "Update Helm dependencies"
+echo "==========================================="
+echo "Make sure to push this new commit upstream!"
+echo "==========================================="
+
+# Tag new images
 docker tag pacman ghcr.io/shuguet/pacman:${NEW_VERSION}
 docker tag pacman ghcr.io/shuguet/pacman:latest
+
+# Login to ghcr.io
 echo ${GITHUB_TOKEN} | docker login ghcr.io -u ${GITHUB_USERNAME} --password-stdin
+
+# Push images to ghcr.io
 docker push ghcr.io/shuguet/pacman:${NEW_VERSION}
 docker push ghcr.io/shuguet/pacman:latest
