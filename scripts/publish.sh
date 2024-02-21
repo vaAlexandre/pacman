a6ad26e6bff2ae849afe784fa2f50fd228e6b6bf
Sylvain Huguet <sylvain.huguet@veeam.com>
2024-02-21 15:50:48 +0100

    Quick fix in publish.sh steps

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,12 +10,13 @@
 #
 # Required tools:
 #   - docker
-docker build . -t pacman
 NEW_VERSION='0.1.12'
 GITHUB_USERNAME='shuguet'
 GITHUB_TOKEN='nice try'
 
-docker tag ghcr.io/shuguet/pacman:${NEW_VERSION} ghcr.io/shuguet/pacman:latest
+docker build . -t pacman
+docker tag pacman ghcr.io/shuguet/pacman:${NEW_VERSION}
+docker tag pacman ghcr.io/shuguet/pacman:latest
 echo ${GITHUB_TOKEN} | docker login ghcr.io -u ${GITHUB_USERNAME} --password-stdin
 docker push ghcr.io/shuguet/pacman:${NEW_VERSION}
 docker push ghcr.io/shuguet/pacman:latest
