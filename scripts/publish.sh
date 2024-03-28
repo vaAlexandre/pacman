44092fc0add8bf9d1f419063ad083ee990fcfc21
Sylvain Huguet <sylvain@huguet.me>
2024-03-28 14:37:54 +0100

    Update Helm dependencies (#22)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,12 +10,9 @@
 #
 # Required tools:
 #   - docker
-NEW_VERSION='0.1.13'
-GITHUB_USERNAME='shuguet'
-GITHUB_TOKEN='nice try'
-
-# Perform local build
-docker build . -t pacman
+# NEW_VERSION='0.1.15'
+# GITHUB_USERNAME='shuguet'
+# GITHUB_TOKEN='nice try'
 
 # Update Helm dependencies (prepare git commit)
 (cd charts/pacman && helm dependency update)
@@ -25,13 +22,13 @@ echo "==========================================="
 echo "Make sure to push this new commit upstream!"
 echo "==========================================="
 
-# Tag new images
-docker tag pacman ghcr.io/shuguet/pacman:${NEW_VERSION}
-docker tag pacman ghcr.io/shuguet/pacman:latest
+# # Tag new images
+# docker tag pacman ghcr.io/shuguet/pacman:${NEW_VERSION}
+# docker tag pacman ghcr.io/shuguet/pacman:latest
 
-# Login to ghcr.io
-echo ${GITHUB_TOKEN} | docker login ghcr.io -u ${GITHUB_USERNAME} --password-stdin
+# # Login to ghcr.io
+# echo ${GITHUB_TOKEN} | docker login ghcr.io -u ${GITHUB_USERNAME} --password-stdin
 
-# Push images to ghcr.io
-docker push ghcr.io/shuguet/pacman:${NEW_VERSION}
-docker push ghcr.io/shuguet/pacman:latest
+# # Push images to ghcr.io
+# docker push ghcr.io/shuguet/pacman:${NEW_VERSION}
+# docker push ghcr.io/shuguet/pacman:latest
